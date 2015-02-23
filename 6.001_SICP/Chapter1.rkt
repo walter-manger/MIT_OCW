83bcc0ae2ad0ef679d7298caaabde6341ef3f364
walter-manger <walter.manger@gmail.com>
2015-02-23 11:10:12 -0700

    Exercise 1.6, explained

--- 6.001_SICP/Chapter1.rkt
+++ 6.001_SICP/Chapter1.rkt
@@ -55,12 +55,60 @@ a)
 ;; Exercise 1.4
 ;;(define (a-plus-abs-b a b)
 ;;  ((if (> b 0) + -) a b))
+
 ;; The if combination determines the operation on a and b
 ;; if b is a negative number, ie less than zero, then the operation
 ;; is the difference between a and -b
 ;; else, the + operator is used
 
 ;; Exercise 1.5
+(define (p) (p))
+(define (test x y)
+(if (= x 0) 0 y))
+(test 0 (p))
 
+;; Applicative-Order: Eval [the arguments] and [then] Apply. 
+;; To apply a compound procedure to arguments, 
+;; evaluate the body of the procedure with each formal
+;; parameter replaced by the corresponding argument.
+
+; Applicative-Order Applied
+; (test 0 (p))
+; (test 0 ((p)))          ; Replace
+; (test 0 (((p))))        ; Replace
+; (test 0 ((((p)))))      ; Replace
+; ...                     ; Replace Forever
+
+; Normal-Order: Fully expand and then reduce
+
+; Normal-Order Applied
+; (test 0 (p))
+; (if (= 0 0) 0 (p))      ; 
+; (if true 0 (p))        
+; 0
+; Short Cicuits because the predicate is evaluated first, 
+; which evaluates to true, therefore the consequent, 
+; 0 in this case, is returned
+
+
+;; Exercise 1.6
+; Original Newton's Method
+;(define (sqrt-iter guess x)
+;  (if (good-enough? guess x)
+;      guess 
+;      (sqrt-iter (improve guess x) x)))
+
+; New If
+;(define (sqrt-iter guess x)
+;  (new-if (good-enough? guess x)
+;          guess
+;          (sqrt-iter (improve guess x) x)))
+
+; In the new-if procedure, Applicative-Order substitution will need to
+; evaluate the call to sqrt-iter since it is an argument, 
+; regardless if it is truly needed or not.
+; As a result, there will be a non-terminated procedure call to sqrt-iter
+
+;; Exercise 1.7
 
 
