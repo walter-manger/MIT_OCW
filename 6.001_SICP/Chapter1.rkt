cd776e8a16d6677be40728648582c1fb184bef62
walter-manger <walter.manger@gmail.com>
2015-02-25 11:14:22 -0700

    Exercise 1.9

--- 6.001_SICP/Chapter1.rkt
+++ 6.001_SICP/Chapter1.rkt
@@ -160,3 +160,32 @@ a)
       (cube-root guess (improve-cube guess x) x)))
   
 (cube-root 0.0 1.0 8)
+
+;; Exercise 1.9
+;(define (+ a b)
+;(if (= a 0) b (inc (+ (dec a) b))))
+
+; Substitution
+; (+ 4 5)
+; (inc (+ 3 5))
+; (inc (inc (+ 2 5)))
+; (inc (inc (inc (+ 1 5))))
+; (inc (inc (inc (inc (+ 0 5)))))
+; (inc (inc (inc (inc 5))))
+; (inc (inc (inc 6)))
+; (inc (inc 7))
+; (inc 8)
+; 9
+; This procedure is a Recursive Process, Recursive Procedure
+
+;(define (+ a b)
+;(if (= a 0) b (+ (dec a) (inc b))))
+
+; Substitution
+; (+ 4 5)
+; (+ 3 6)
+; (+ 2 7)
+; (+ 1 8)
+; (+ 0 9)
+; 9
+; This procedure is a Iterative Process, Recursive Procedure
