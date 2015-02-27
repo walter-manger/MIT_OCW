4dea81c17b7b0830f9cd16aa076702016c7d5ccf
walter-manger <walter.manger@gmail.com>
2015-02-27 12:31:50 -0700

    Exercise 1.10 - forgot the last portion

--- 6.001_SICP/Chapter1.rkt
+++ 6.001_SICP/Chapter1.rkt
@@ -325,8 +325,9 @@ a)
 ; Mathematical Definitions
 ; (define (f n) (A 0 n)) => 2n
 ; (define (g n) (A 1 n)) => 2^n
-; (define (h n) (A 2 n)) => 2^  2^  2^    2^    2
+; (define (h n) (A 2 n)) => 2   ^2  ^2    ^2    ^2
 ;                           [1] [2] [3]...[n-1] [n]
+; (define (k n) (* 5 n n)) => 5n^2
 
 
 
